99c2f602cc21511197cefa034b84438d4e4914b9
Gabriel Leite Tecco <111099300+GabrieLeiTecco@users.noreply.github.com>
2024-03-27 17:42:03 -0300

    Add files via upload

---
+++ src/moduleB.js
@@ -0,0 +1,9 @@
+module.exports.obj = {
+    coincidencia(num, numAlea){
+        if (num == numAlea) {
+            console.log(`Parabéns!\n num = ${num}, aleatorio = ${numAlea}`)
+        } else {
+            console.log('Não foi dessa vez....')
+        }
+    }
+}
