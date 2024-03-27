e5c90a27e3e18099fdf398eec407470cfcdfe000
Gabriel Leite Tecco <111099300+GabrieLeiTecco@users.noreply.github.com>
2024-03-27 17:56:18 -0300

    Update mainClient.js

--- src/mainClient.js
+++ src/mainClient.js
@@ -1,6 +1,6 @@
 const moduloA = require('./moduleA') // transforma o conteudo de outro arquivo em um objeto nesse arquivo
 const moduloB = require('./moduleB')
 
-numAlea = moduloA.randomico.randomico(1,300)
+numAlea = moduloA.randomico(1,300)
 
-moduloB.obj.coincidencia(2, numAlea)
+moduloB.obj(2, numAlea)
