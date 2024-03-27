fbfa0dab842e3628feb174ce1b3b3798372cc3e8
Gabriel Leite Tecco <111099300+GabrieLeiTecco@users.noreply.github.com>
2024-03-27 17:15:04 -0300

    Add files via upload

---
+++ src/mainClient.js
@@ -0,0 +1,9 @@
+const moduloA = require('./moduleA') // transforma o conteudo de outro arquivo em um objeto nesse arquivo
+
+console.log(moduloA.randomico.randomico(1,200))
+console.log(moduloA.randomico.randomico(1,200))
+console.log(moduloA.randomico.randomico(1,200))
+console.log(moduloA.randomico.randomico(1,200))
+console.log(moduloA.randomico.randomico(1,200))
+console.log(moduloA.randomico.randomico(1,200))
+console.log(moduloA.randomico.randomico(1,200))
---
+++ src/moduleA.js
@@ -0,0 +1,5 @@
+module.exports.randomico = {
+    randomico(min, max){
+        let num = Math.floor(Math.random() * (max-min) + min)
+        return num
+}}
